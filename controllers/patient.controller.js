c9055987f73a0877711bd1157cfd028b685e879d
sachin-source <sachinmgvt@gmail.com>
2023-03-23 18:53:31 +0530

    patient code incomplete

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -1,11 +1,16 @@
 const patient = require("../models/patient");
 
-const getPatientList = (req, res) => {
+const patientList = (req, res) => {
     const { name, age, disease, contactNumber, appointmentDate, appointmentDetails } = req.body;
     return patient.create( { name, age, disease, contactNumber, appointmentDate, appointmentDetails } ).then((d) => {
         patient.find({name : "name"}, (err, patients)=>{
             res.send({ status:!err, patients })
         })
+    })
+}
+const getPatientList = (req, res) => {
+    return patient.find({}, null, {sort: {createdAt: -1}}, (err, patients)=>{
+            res.send({ status:!err, patients })
     })
 }
 
