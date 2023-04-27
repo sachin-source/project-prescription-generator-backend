167cd804d648c4e3616f06a43703cc37c2a556cc
sachin-source <sachinmgvt@gmail.com>
2023-04-27 10:32:08 +0530

    patient list api

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -2,10 +2,8 @@ const patient = require("../models/patient");
 
 const getPatientList = (req, res) => {
     const { name, age, disease, contactNumber, appointmentDate, appointmentDetails } = req.body;
-    return patient.create( { name, age, disease, contactNumber, appointmentDate, appointmentDetails } ).then((d) => {
-        patient.find({name : "name"}, (err, patients)=>{
-            res.send({ status:!err, patients })
-        })
+    return patient.find({}, (err, patients)=>{
+        res.send({ status:!err, patients })
     })
 }
 
